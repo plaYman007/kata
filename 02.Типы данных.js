207edb8516b82f0f536eb7250a6046861ba73161
Andrey <example@gmail.com>
2024-10-29 13:03:01 +0300

    first commit

---
+++ 02.Типы данных.js
@@ -0,0 +1,46 @@
+"use strict";
+//1)Простые типы
+//Числовой тип данных number
+let number = 4;
+console.log(number + number);
+
+//Строчный тип данных
+const person = "Alex";
+
+//Логический тип данных
+const bool = true;
+
+// null - когда чего-то просто не существует
+console.log(something);
+null;
+
+//undefiend - когда что-то существует, но у него нет значения
+let und;
+console.log(und);
+
+//2)Объекты. Структура хранения данных в парном порядке Ключ:значение
+const obj = {
+  name: "John",
+  age: 25,
+  isMarried: false,
+};
+console.log(obj.age);
+console.log(obj.name);
+
+//Массивы - более комплексный тип данных, не являющийся отдельным типом
+//Массив - перечень информации ПО ПОРЯДКУ
+let array = ["plum.png", "orange.jpg", 6, Alex, "apple.bmp"];
+console.log(
+  array[1] /*Ставим то число, под которым идёт необходимый элемент в массиве. ОТСЧЁТ ИДЁТ С НУЛЯ! прим. если хотим orange.jpg, то пишем цифру 1*/
+);
+const storeName = "Bakka";
+const storeDescription = {
+  budget: 10000,
+  employees: ["Ba", "kk", "a"],
+  products: {
+    tovar1: 2500,
+    tovar2: 3000,
+  },
+  open: true,
+};
+console.log(storeDescription);
---
+++ 03.Всплывающие окна.js
@@ -0,0 +1,11 @@
+"use strict";
+//alert("hello pidr");
+//const result = confirm("Are you ahueli tam?"); console.log(result);
+/*const answer = prompt("Тебе сука с правой?", "");
+console.log(typeof answer);*/
+const answers = [];
+answers[0] = prompt("Как звать тя сука", "");
+answers[1] = prompt("По бате как", "");
+answers[2] = prompt("ебаться можешь уже", "");
+document.write(answers);
+//com: promrt,document можно запустить ТОЛЬКО ВНУТРИ БРАУЗЕРА
